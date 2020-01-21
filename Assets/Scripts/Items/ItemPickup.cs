e75f6d700c6df9e391a587a7a7bb244067587809
Radusek <Radusek@github.com>
2020-01-21 19:30:43 +0100

    Spawning bug fix, items fly towards the player

--- Assets/Scripts/Items/ItemPickup.cs
+++ Assets/Scripts/Items/ItemPickup.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class ItemPickup : Interactable
 {
@@ -8,12 +9,37 @@ public class ItemPickup : Interactable
 
     public override void Interact(GameObject interactor)
     {
-        Inventory playerInventory = interactor.GetComponent<Inventory>();
-        if (!playerInventory.CanPickItem(item))
+        Inventory interactorInventory = interactor.GetComponent<Inventory>();
+        if (!interactorInventory.CanPickItem(item))
             return;
 
+        StartCoroutine(FlyTowardsInteractorCoroutine(interactorInventory));
+    }
+
+    private IEnumerator FlyTowardsInteractorCoroutine(Inventory interactorInventory)
+    {
+        SphereCollider collider = GetComponent<SphereCollider>();
+        collider.enabled = false;
+        GameObject interactor = interactorInventory.gameObject;
+        float distanceTreshold = 0.1f;
+        float movingSpeed = 5f;
+        while ((interactor.transform.position - transform.position).sqrMagnitude > distanceTreshold * distanceTreshold)
+        {
+            Vector3 relativeVector = interactor.transform.position - transform.position;
+            relativeVector.y = 0f;
+            transform.Translate(movingSpeed * relativeVector.normalized * Time.deltaTime, Space.World);
+            transform.LookAt(interactor.transform);
+            yield return null;
+        }
+
+        if (!interactorInventory.CanPickItem(item))
+        {
+            collider.enabled = true;
+            yield break;
+        }
+
         item.owner = interactor;
-        playerInventory.AddItem(item);
+        interactorInventory.AddItem(item);
         Destroy(gameObject);
     }
 }
