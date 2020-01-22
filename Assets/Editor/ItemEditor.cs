41c4eefbfa9807044edd1275e513613bac2afe49
Radusek <Radusek@github.com>
2020-01-22 20:14:21 +0100

    WeaponItem custom inspector

---
+++ Assets/Editor/ItemEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Item))]
+public class ItemEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        Item item = (Item)target;
+
+        item.itemName = EditorGUILayout.TextField("Item Name", item.itemName);
+        item.itemDescription = EditorGUILayout.TextField("Item Description", item.itemDescription);
+
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.PrefixLabel("Icon");
+        item.icon = (Sprite)EditorGUILayout.ObjectField(item.icon, typeof(Sprite), allowSceneObjects: true);
+        EditorGUILayout.EndHorizontal();
+
+        item.stackLimit = EditorGUILayout.IntField("Stack Limit", item.stackLimit);
+    }
+}
---
+++ Assets/Editor/WeaponItemEditor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(WeaponItem))]
+public class WeaponItemEditor : ItemEditor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        WeaponItem weaponItem = (WeaponItem)target;
+
+        weaponItem.attackType = (AttackType)EditorGUILayout.EnumPopup("Attack Type", weaponItem.attackType);
+
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.PrefixLabel("Firing Sound");
+        weaponItem.firingSound = (AudioClip)EditorGUILayout.ObjectField(weaponItem.firingSound, typeof(AudioClip), allowSceneObjects: true);
+        EditorGUILayout.EndHorizontal();
+
+        weaponItem.damage = EditorGUILayout.IntField("Damage", weaponItem.damage);
+        weaponItem.recastInterval = EditorGUILayout.FloatField("Recast Interval", weaponItem.recastInterval);
+
+        if (weaponItem.attackType != AttackType.Melee)
+        {
+            weaponItem.projectileType = (PoolType)EditorGUILayout.EnumPopup("Projectile Type", weaponItem.projectileType);
+            weaponItem.projectileSpeed = EditorGUILayout.FloatField("Projectile Speed", weaponItem.projectileSpeed);
+        }
+    }
+}
