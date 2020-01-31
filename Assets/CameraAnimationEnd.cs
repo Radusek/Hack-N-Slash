73c437ecd50c28b7b3d8bdb46410a9a7083bb234
Radusek <Radusek@github.com>
2020-01-31 22:33:56 +0100

    Items placed, character selection info added

---
+++ Assets/CameraAnimationEnd.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAnimationEnd : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject objectToEnable;
+
+
+    public void EnableObject()
+    {
+        objectToEnable.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Items/InteractableSign.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableSign : Interactable
+{
+    [SerializeField]
+    [TextArea]
+    private string content;
+
+
+    public override string GetTooltipText()
+    {
+        return content;
+    }
+}
