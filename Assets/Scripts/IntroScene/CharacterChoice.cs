c140b3f299dbf6e68ae7045f47cbaa48f6adc4ac
Radusek <Radusek@github.com>
2020-01-31 16:27:30 +0100

    Character selection scripts and ligting update

---
+++ Assets/Scripts/IntroScene/CharacterChoice.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterChoice : MonoBehaviour
+{
+    [SerializeField]
+    private int classId;
+
+    private static AsyncOperation sceneLoadingOp;
+
+    private void OnMouseDown()
+    {
+        if (sceneLoadingOp != null)
+            return;
+
+        CharacterSelected.playerClass = classId;
+        StartCoroutine(StartLoadingSceneCoroutine());
+    }
+
+    private IEnumerator StartLoadingSceneCoroutine()
+    {
+        sceneLoadingOp = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        //sceneLoadingOp.allowSceneActivation = false;
+
+        while (!sceneLoadingOp.isDone)
+            yield return null;
+
+        Debug.Log("New Scene Loaded");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            if(sceneLoadingOp != null)
+                sceneLoadingOp.allowSceneActivation = true;
+        }
+    }
+}
+
+public static class CharacterSelected
+{
+    public static int playerClass = 0;
+}
---
+++ Assets/Scripts/World/CharacterSelector.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CharacterSelector : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] characters;
+
+
+    private void Awake()
+    {
+        characters[CharacterSelected.playerClass].SetActive(true);
+    }
+}
